06cebb6227fc746378876b917666c358f1f9852f
CecoM <ceco_milchev@abv.bg>
2017-10-13 16:14:46 +0300

    Added properties to Person Class

--- TKL-Research/Models.Framework/Person.cs
+++ TKL-Research/Models.Framework/Person.cs
@@ -8,5 +8,12 @@ namespace Models.Framework
 {
     public class Person
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
     }
 }
